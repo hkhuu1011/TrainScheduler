73053aa855eb4e310f5bc5e0fc3499afd30b0854
Han Khuu <h.khuu01@gmail.com>
2017-03-13 20:01:50 -0400

    initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDU6KLUvk0XpoBQxsvxTe-VSbIFNY0qt0g",
+    authDomain: "trainscheduler-4cfa6.firebaseapp.com",
+    databaseURL: "https://trainscheduler-4cfa6.firebaseio.com",
+    storageBucket: "trainscheduler-4cfa6.appspot.com",
+    messagingSenderId: "709446553883"
+  };
+  firebase.initializeApp(config);
+
+  
